f1752a2ee8bd390517a8a0e8ffe5e5c324b1c4b0
Ajiwunmi <ajiwunmi@gmail.com>
2025-06-10 17:52:14 +0100

    tailwind config file set up
    Signed-off-by: Ajiwunmi <ajiwunmi@gmail.com>

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+	content: ["./src/**/*.{js,jsx}", "./public/index.html"],
+	
+	plugins: [],
+	theme: {
+		extend: {
+			colors: {
+				happy: "#34D399", // green-400
+				neutral: "#D1D5DB", // gray-300
+				sad: "#F87171", // red-400
+			},
+		},
+	},
+};
