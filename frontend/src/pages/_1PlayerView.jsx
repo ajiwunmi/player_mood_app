df2c80417d4595ed098db45aa09c8bf647230e41
Ajiwunmi <ajiwunmi@gmail.com>
2025-06-11 17:35:37 +0100

    frontend pages added

---
+++ frontend/src/pages/_1PlayerView.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import EmojiButton from "../components/EmojiButton";
+import ThankYouMessage from "../components/ThankYouMessage";
+import { postMood } from "../Api";
+
+export default function PlayerView() {
+	const [sent, setSent] = useState(false);
+	const handleMood = async (emoji) => {
+		try {
+
+            alert("Submitting mood: " + emoji);
+            console.log("Submitting mood:", emoji);
+			await postMood(emoji);
+          	setSent(true);
+			setTimeout(() => setSent(false), 2000);
+		} catch (e) {
+			console.error(e);
+			alert("Error submitting mood" + e.message);
+		}
+	};
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			{sent ? (
+				<ThankYouMessage />
+			) : (
+				<div className="flex space-x-8">
+					<EmojiButton
+						emoji="😃"
+						label="happy"
+						onClick={() => handleMood("happy")}
+					/>
+					<EmojiButton
+						emoji="😐"
+						label="neutral"
+						onClick={() => handleMood("neutral")}
+					/>
+					<EmojiButton
+						emoji="😞"
+						label="sad"
+						onClick={() => handleMood("sad")}
+					/>
+				</div>
+			)}
+		</div>
+	);
+}
